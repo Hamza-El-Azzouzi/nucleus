ea04f890ddf32037178479d063588905f9ceed85
Hamza Maach <hamzamaach56@gmail.com>
2025-06-02 16:04:11 +0100

    remove file

--- src/builtins/rm.rs
+++ src/builtins/rm.rs
@@ -1,5 +1,33 @@
+use std::{env, fs, path::PathBuf};
+
 pub fn rm(args: Vec<String>, recursive: bool) {
     if args.is_empty() {
         println!("missing operand");
+    }
+
+    // get the current dir
+    let cur_dir = match env::current_dir() {
+        Ok(dir) => dir,
+        Err(err) => {
+            println!("Error getting current directory: {}", err);
+            return;
+        }
+    };
+
+    for elem in args {
+        // check if the elements of the args are valid and exist
+        let path = cur_dir.join(&elem);
+        if !path.exists() {
+            eprintln!("rm: cannot remove '{}': No such file or directory", elem);
+            continue;
+        }
+
+        if path.is_file() {
+            if let Err(err) = fs::remove_file(&path) {
+                println!("Failed to remove file '{}': {}", elem, err);
+                return;
+            }
+        } else if path.is_dir() {
+        }
     }
 }
