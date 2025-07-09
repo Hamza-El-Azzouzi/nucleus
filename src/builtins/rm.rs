f017dd8e9bb5d1c0404198926f1dcdba708e265a
Hamza Maach <hamzamaach56@gmail.com>
2025-07-09 17:49:21 +0100

    remove comments

--- src/builtins/rm.rs
+++ src/builtins/rm.rs
@@ -1,7 +1,6 @@
 use std::{env, fs};
 
 pub fn rm(args: Vec<String>, recursive: bool) {
-    // get the current dir
     let cur_dir = match env::current_dir() {
         Ok(dir) => dir,
         Err(err) => {
@@ -19,7 +18,6 @@ pub fn rm(args: Vec<String>, recursive: bool) {
             continue;
         }
 
-        // check if the elements of the args are valid and exist
         let path = cur_dir.join(&elem);
         if !path.exists() {
             eprintln!("rm: cannot remove '{}': No such file or directory", elem);
