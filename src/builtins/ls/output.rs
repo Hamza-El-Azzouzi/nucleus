ff5db6f23d466c66e99e59a21e5ef050b64ced00
khlifihamza <khllifihamza66@gmail.com>
2025-07-12 17:21:40 +0100

    format ls

--- src/builtins/ls/output.rs
+++ src/builtins/ls/output.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use regex::Regex;
+use term_grid::{Cell, Direction, Filling, Grid, GridOptions};
 use terminal_size::{Width, terminal_size};
 
 use super::{Directory, formatter::format_detailed_file_info, parser::Flag};
@@ -42,15 +44,48 @@ impl LsOutput {
         }
     }
 
-    fn format_result(result: &Vec<Vec<String>>, _term_width: &usize) -> String {
-        let mut res = String::new();
-        for (i, path) in result.iter().enumerate() {
-            res.push_str(&path[0]);
-            if i < result.len() - 1 {
-                res.push_str("  ");
-            }
+    fn strip_ansi_codes(s: &str) -> String {
+        let re = Regex::new(r"\x1b\[[0-9;]*m").unwrap();
+        re.replace_all(s, "").to_string()
+    }
+
+    fn format_result(result: &Vec<Vec<String>>, term_width: usize) -> String {
+        let mut grid = Grid::new(GridOptions {
+            filling: Filling::Spaces(2),
+            direction: Direction::TopToBottom,
+        });
+
+        let max_width = result
+            .iter()
+            .filter_map(|entry| entry.first())
+            .map(|name| Self::strip_ansi_codes(name).len())
+            .max()
+            .unwrap_or(0);
+
+        for s in result.iter() {
+            let clean_name = Self::strip_ansi_codes(&s[0]);
+            grid.add(Cell::from(clean_name.as_str()));
         }
-        res.push_str("\n");
+        let col_width = max_width + 2; // max width + 2 spaces
+        let max_cols = if col_width == 0 {
+            1
+        } else {
+            (term_width / col_width).max(1)
+        };
+        println!("{max_cols}");
+        let mut res = String::new();
+        res.push_str(&grid.fit_into_columns(max_cols).to_string());
+        // if grid.fit_into_width(term_width).is_some() {
+        //     res.push_str(&grid.fit_into_columns(14).to_string());
+        // } else {
+        //     for (i, path) in result.iter().enumerate() {
+        //         res.push_str(&path[0]);
+        //         if i < result.len() - 1 {
+        //             res.push_str("  ");
+        //         }
+        //     }
+        // }
+        res.push('\n');
         res
     }
 
@@ -70,7 +105,7 @@ impl LsOutput {
             for path in result.iter() {
                 println!(
                     "{}",
-                    format_detailed_file_info(&max_lens, path, &max_size_len)
+                    format_detailed_file_info(&max_lens, path, max_size_len)
                 );
             }
         } else {
@@ -80,7 +115,7 @@ impl LsOutput {
                 100
             };
 
-            let res = Self::format_result(result, &term_width);
+            let res = Self::format_result(result, term_width);
 
             print!("{res}");
         }
